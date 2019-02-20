11da2ffbacaafafeaa6b40f3639fc559c20fd154
정기욱 <rldnr56@me.com>
2019-02-20 16:32:27 +0900

    피커에 들어갈 toolbar 객체를 정의해준다.
    1. toolbar가 피커에 정해진 영역이 있으므로 높이와 색상만 정해주면된다.
    
    2. inputAccessoryView를 이용해 툴바를 피커 뷰에 추가하고
    
    툴바에 들어갈 닫기 버튼을 구현한 후 추가해준다.
    
    3. Done을 눌렸을때 피커가 꺼질수 있도록 정의해준다.

--- PlistCustom/ListViewController.swift
+++ PlistCustom/ListViewController.swift
@@ -27,6 +27,23 @@ class ListViewController : UITableViewController, UIPickerViewDelegate, UIPicker
         // account 텍스트 필드 입력 방식을 가상 키보드 대신 피커 뷰로 설정
         self.account.inputView = picker
         
+        // 툴 바 객체 정의 ( 피커가 악세사리 뷰 영역에 Done버튼을 추가하기 위해 )
+        let toolbar = UIToolbar()
+        toolbar.frame = CGRect(x: 0, y: 0, width: 0, height: 35)
+        toolbar.barTintColor = UIColor.lightGray
+        
+        // 액세서리 뷰 영역에 툴 바를 표시
+        self.account.inputAccessoryView = toolbar
+        
+        // 툴 바에 들어갈 닫기 버튼
+        let done = UIBarButtonItem()
+        done.title = "Done"
+        done.target = self
+        done.action = #selector(pickerDone)
+        
+        // 버튼을 툴 바에 추가
+        toolbar.setItems([done], animated: true)
+        
     }
     
     // 생성할 컴포넌트의 개수를 정의합니다. (보통은 1개지만 3개 이하까지는 할 수 있다. )
@@ -56,6 +73,10 @@ class ListViewController : UITableViewController, UIPickerViewDelegate, UIPicker
         self.account.text = account
         
         // 2. 입력 뷰를 닫음
+        // self.view.endEditing(true)
+    }
+    
+    @objc func pickerDone(_ sender: Any){
         self.view.endEditing(true)
     }
 }
